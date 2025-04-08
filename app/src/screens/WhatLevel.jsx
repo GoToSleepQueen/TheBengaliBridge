a35551374465681dbaf24eff62e332916745fec2
GoToSleepQueen <oishani.potter@gmail.com>
2025-04-08 18:17:24 -0500

    i sort of did the what level page

--- app/src/screens/WhatLevel.jsx
+++ app/src/screens/WhatLevel.jsx
@@ -1,6 +1,8 @@
 import { StyleSheet, Text, View, TouchableOpacity, SafeAreaView } from 'react-native'
 import React from 'react'
 import { useNavigation } from '@react-navigation/native'
+import { Feather, FontAwesome6 } from '@expo/vector-icons'
+import IconButton from '../components/IconButton'
 
 const WhatLevel = () => {
   const navigation = useNavigation()
@@ -12,18 +14,29 @@ const WhatLevel = () => {
   }
   return (
     <SafeAreaView style={styles.container}>
-      <Text>WhatLevel</Text>
-      <TouchableOpacity
-      style={styles.button}
-      onPress={gotoAccountCreation}>
-        <Text>Next</Text>
-      </TouchableOpacity>
-      
-      <TouchableOpacity
-      style={styles.button}
-      onPress={gotoWhyLearning}>
-        <Text>Back</Text>
-      </TouchableOpacity>
+      <Text style={styles.text}>What is your level of fluency?</Text>
+      <View style={styles.buttonsContainer}>
+        <IconButton buttonText="Beginner" 
+        icon={<FontAwesome6 name="solid-circle" size={15} color={"white"}></FontAwesome6>} 
+        shadowColor="#ff3434" 
+        buttonColor="#b22222" 
+        onPress={gotoAccountCreation} />
+        <IconButton buttonText="Intermediate" 
+        icon={<FontAwesome6 name="circle" size={15} color={"white"}></FontAwesome6>}
+        shadowColor="#ff3434" 
+        buttonColor="#b22222" 
+        onPress={gotoAccountCreation} />
+        <IconButton buttonText="Advanced" 
+        icon={<FontAwesome6 name="circle" size={15} color={"white"}></FontAwesome6>}
+        shadowColor="#ff3434" 
+        buttonColor="#b22222" 
+        onPress={gotoAccountCreation} />
+      </View>
+      <IconButton style={styles.backButton} buttonText="Back" 
+      icon={<Feather name="arrow-left" size={25} color={"white"}></Feather>} 
+      shadowColor="#ff3434" 
+      buttonColor="#b22222" 
+      onPress={gotoWhyLearning} />
     </SafeAreaView>
   )
 }
@@ -33,6 +46,26 @@ export default WhatLevel
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fdf6e3",
-  }
+    backgroundColor: '#fdf6e3',
+    alignItems: 'center',
+    position: 'relative',
+  },
+  text: {
+    fontWeight: "bold",
+    fontSize: 24,
+    padding: 5,
+    textAlign: 'center'
+  },
+  buttonsContainer: {
+    width: '70%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+  },
+  backButton: {
+    position: 'absolute',
+    bottom: 50,
+    paddingBottom: 15,
+    width: '80%',
+  },
 })
