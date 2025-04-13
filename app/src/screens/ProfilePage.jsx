ddbd235a02032c3999c898147592d7939733efd8
Dragon Screech <dragon.screech76@gmail.com>
2025-04-13 09:58:47 -0500

    added a sign out button to the profile page

--- app/src/screens/ProfilePage.jsx
+++ app/src/screens/ProfilePage.jsx
@@ -1,10 +1,24 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import Button from '../components/Button'
+import { signOut } from 'firebase/auth'
+import { auth } from '../config/firebaseConfig'
+import { useNavigation } from '@react-navigation/native'
 
 const ProfilePage = () => {
+  const navigation = useNavigation()
+  const handleSignOut = async () => {
+    await signOut(auth).then(() => {
+      console.log("User signed out")
+    }).catch((error) => {
+      console.error("Error signing out: ", error)
+    })
+    navigation.navigate("Log In")
+  }
   return (
     <View>
       <Text>ProfilePage</Text>
+      <Button buttonText={"Sign Out"} shadowColor="#ff3434" buttonColor="#b22222" onPress={handleSignOut}></Button>
     </View>
   )
 }
