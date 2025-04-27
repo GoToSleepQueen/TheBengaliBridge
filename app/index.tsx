d636b1ba55eabc08cb2e7b6618c0312e0d2e9837
GoToSleepQueen <oishani.potter@gmail.com>
2025-04-27 11:51:45 -0500

    what the meower

--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,31 @@
 import "../gesture-handler"
-import React from "react"
+import React, { useContext } from "react"
 import { View, Text, SafeAreaView, StyleSheet } from "react-native"
 import AppNavigator from './src/components/AppNavigator'
 import { UserProvider } from './src/context/UserContext';
-import { AuthProvider } from "./src/context/AuthContext";
+import AuthContext, { AuthProvider } from "./src/context/AuthContext";
+import LoadingScreen from "./src/screens/LoadingScreen";
+console.log("RENDER: NavigationContainer hit")
+import { NavigationContainer } from "@react-navigation/native";
 
 const App = () => {
+
   return (
     <AuthProvider>
       <UserProvider>
-        <AppNavigator></AppNavigator>
+        <AppInner />
       </UserProvider>
     </AuthProvider>
-  )
-}
+  );
+};
+const AppInner = () => {
+  const { loading } = useContext(AuthContext);
+
+  if (loading) return <LoadingScreen/>;
+
+  return <AppNavigator/>;
+};
+
 const styles = StyleSheet.create({
 })
 export default App
--- app/src/components/AppNavigator.jsx
+++ app/src/components/AppNavigator.jsx
@@ -1,6 +1,5 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React, { useState } from 'react'
-import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import StarterPage from '../screens/StarterPage'
 import WhyLearning from '../screens/WhyLearning'
@@ -14,58 +13,22 @@ import { AuthContext, AuthProvider } from '../context/AuthContext' // Import Aut
 
 const Stack = createStackNavigator();
 
-
 const AppNavigator = () => {
-  const { loading, user } = useContext(AuthContext); // Get loading and user from AuthContext
-
   return (
     <Stack.Navigator
-      screenOptions={{
-        headerShown: false,
-      }}>
-      <Stack.Screen
-        name='Start'
-        component={StarterPage}
-        options={{ headerShown: false }}>
-      </Stack.Screen>
-
-      <Stack.Screen
-        name='Log In'
-        component={LogIn}
-      >
-      </Stack.Screen>
-
-      <Stack.Screen
-        name="Why Learning"
-        component={WhyLearning}>
-      </Stack.Screen>
-
-      <Stack.Screen
-        name="What Level"
-        component={WhatLevel}>
-      </Stack.Screen>
-
-      <Stack.Screen
-        name="Account Creation"
-        component={AccountCreation}>
-      </Stack.Screen>
-
-      {loading ? <Stack.Screen
-        name="Loading"
-        component={LoadingScreen}>
-      </Stack.Screen> : user ? (
-        <Stack.Screen
-          name="Tabs"
-          component={Tabs}>
-        </Stack.Screen>
-      ) : (
-        <Stack.Screen
-          name="Log In"
-          component={LogIn}>
-        </Stack.Screen>
-      )}
+      screenOptions={{ headerShown: false }}
+      initialRouteName="Start"
+    >
+      <Stack.Screen name="Start" component={StarterPage} />
+      <Stack.Screen name="Why Learning" component={WhyLearning} />
+      <Stack.Screen name="What Level" component={WhatLevel} />
+      <Stack.Screen name="Account Creation" component={AccountCreation} />
+      <Stack.Screen name="Log In" component={LogIn} />
+      <Stack.Screen name="Tabs" component={Tabs} />
+      <Stack.Screen name="Loading" component={LoadingScreen} />
     </Stack.Navigator>
-  )
-}
+  );
+};
+
 
 export default AppNavigator
