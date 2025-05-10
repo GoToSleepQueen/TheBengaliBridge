cb60b55fbee68a7de3bd27d687bb8aca0afe7e6d
GoToSleepQueen <oishani.potter@gmail.com>
2025-05-10 10:31:15 -0500

    added letter tile component, tried to fix dependencies

---
+++ app/src/components/LetterTile.jsx
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { TouchableOpacity } from 'react-native'
+
+const LetterTile = ({ shadowColor, buttonColor, onPress, buttonText, buttonStyle, disabled }) => {
+    return (
+      <View style={[styles.buttonStyle]}/>
+    )
+}
+
+export default LetterTile
